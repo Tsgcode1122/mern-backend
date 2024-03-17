4f3e2148acd50cb37e016dca56752d87d397bc67
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-03-17 13:10:39 +0100

    new

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const roomsRoute = require("./routes/roomsRoute");
 const usersRoute = require("./routes/usersRoute");
 const bookingsRoute = require("./routes/bookingsRoute");
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://tsghotelbackend.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  }),
+);
 app.use("/api/rooms", roomsRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/bookings", bookingsRoute);
