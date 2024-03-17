58eabe306d56b8a1fd6d7c8fe839dfb651be6b52
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-03-17 12:11:57 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+const roomModel = require("./models/room");
+const dbConfig = require("./db");
+const roomsRoute = require("./routes/roomsRoute");
+const usersRoute = require("./routes/usersRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
+app.use(express.json());
+app.use(cors());
+app.use("/api/rooms", roomsRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/bookings", bookingsRoute);
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => console.log(`Node Server Started using ${port}`));
